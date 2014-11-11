5e84c0c8c7dfdd4e53e98d67a1ff65e3cd5402e8
Hadrien DELAITRE <hadrien.deuxlettres@gmail.com>
2014-11-11 13:28:49 +0100

    Update R.Window.js

--- src/R.Window.js
+++ src/R.Window.js
@@ -1,6 +1,5 @@
 module.exports = function(R) {
   const _ = require('lodash');
-  const assert = require('assert');
   const should = R.should;
 
   const defaultParams = {
@@ -10,25 +9,27 @@ module.exports = function(R) {
       scrollLeft: 0,
   };
 
-  return (storeName, dispatcherName, eventEmitterName, params) => {
+  return ({storeName, dispatcherName, eventEmitterName, params}) => {
+    _.defaults(params, defaultParams);
+    
     class Window extends R.App.Plugin {
       constructor({ flux, window, req }){
         super();
         this.storeName = storeName;
         this.dispatcherName = dispatcherName;
         this.eventEmitterName = eventEmitterName;
-        this.params = params || {};
-        _.defaults(this.params, defaultParams);
-
+        this.params = params;
+        
+        let store = flux.getStore(this.storeName);
+        let eventEmitter = flux.getEventEmitter(this.eventEmitterName);
+        
         if(window) {
           // Client-only init
           flux.getDispatcher(this.dispatcherName).addActionListener('/Window/scrollTo', (params) => {
             return _.copromise(function* () {
               _.dev(() =>
                 params.should.be.an.Object &&
-                params.top.should.be.ok &&
                 params.top.should.be.a.Number &&
-                params.left.should.be.ok && 
                 params.left.should.be.a.Number
                 );
               window.scrollTo(params.top, params.left);
@@ -36,32 +37,32 @@ module.exports = function(R) {
             }, this);
           });
           window.addEventListener('scroll', () => {
-            flux.getStore(this.storeName).set('/Window/scrollTop', window.scrollTop);
-            flux.getStore(this.storeName).set('/Window/scrollLeft', window.scrollLeft);
-            flux.getEventEmitter(this.eventEmitterName).emit('/Window/scroll', {
+            store.set('/Window/scrollTop', window.scrollTop);
+            store.set('/Window/scrollLeft', window.scrollLeft);
+            eventEmitter.emit('/Window/scroll', {
               scrollTop: window.scrollTop,
               scrollLeft: window.scrollLeft,
             });
           });
           window.addEventListener('resize', () => {
-            flux.getStore(this.storeName).set('/Window/height', window.innerHeight);
-            flux.getStore(this.storeName).set('/Window/width', window.innerWidth);
-            flux.getEventEmitter(this.eventEmitterName).emit('/Window/resize', {
+            store.set('/Window/height', window.innerHeight);
+            store.set('/Window/width', window.innerWidth);
+            eventEmitter.emit('/Window/resize', {
               height: window.innerHeight,
               width: window.innerWidth,
             });
           });
-          flux.getStore(this.storeName).set('/Window/height', window.innerHeight);
-          flux.getStore(this.storeName).set('/Window/width', window.innerWidth);
-          flux.getStore(this.storeName).set('/Window/scrollTop', window.scrollTop);
-          flux.getStore(this.storeName).set('/Window/scrollLeft', window.scrollLeft);
+          store.set('/Window/height', window.innerHeight);
+          store.set('/Window/width', window.innerWidth);
+          store.set('/Window/scrollTop', window.scrollTop);
+          store.set('/Window/scrollLeft', window.scrollLeft);
         }
         else {
           // Server-only init
-          flux.getStore(this.storeName).set('/Window/height', this.params.height);
-          flux.getStore(this.storeName).set('/Window/width', this.params.width);
-          flux.getStore(this.storeName).set('/Window/scrollTop', this.params.scrollTop);
-          flux.getStore(this.storeName).set('/Window/scrollLeft', this.params.scrollLeft);
+          store.set('/Window/height', this.params.height);
+          store.set('/Window/width', this.params.width);
+          store.set('/Window/scrollTop', this.params.scrollTop);
+          store.set('/Window/scrollLeft', this.params.scrollLeft);
         }
       }
 
